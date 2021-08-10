ad5bc1c96d09033e2e20064deacc40c50ac99ba8
khacduy91 <vukhacduy2709@gmail.com>
2021-08-10 11:01:21 +0700

    3rd commit

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -6,7 +6,7 @@ import Header from "./Header";
 const Layout = ({ children }) => (
   <>
     <Head>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Sun Tropical Phú Quốc</title>
