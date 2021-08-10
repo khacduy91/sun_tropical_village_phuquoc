f23beb9fdfd8cc68805d87464ce69c7ecfe8f8ad
khacduy91 <vukhacduy2709@gmail.com>
2021-08-10 11:46:49 +0700

    comment

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,24 @@
-// import Layout from "../components/Layout/Layout";
-// import AboutKemBeach from "../components/HomePage/AboutKemBeach";
-// import BillComment from "../components/HomePage/BillComment";
-// import Utilities from "../components/HomePage/Utilities";
-// import Slider from "../components/HomePage/Slider";
-// import Project from "../components/HomePage/Project";
-// import Design from "../components/HomePage/Design";
-// import Sungroup from "../components/HomePage/Sungroup";
-// import Form from "../components/HomePage/Form";
+import Layout from "../components/Layout/Layout";
+import AboutKemBeach from "../components/HomePage/AboutKemBeach";
+import BillComment from "../components/HomePage/BillComment";
+import Utilities from "../components/HomePage/Utilities";
+import Slider from "../components/HomePage/Slider";
+import Project from "../components/HomePage/Project";
+import Design from "../components/HomePage/Design";
+import Sungroup from "../components/HomePage/Sungroup";
+import Form from "../components/HomePage/Form";
 
 export default function Home() {
   return (
-    // <Layout>
-    //   <Slider />
-    //   <AboutKemBeach />
-    //   <BillComment />
-    //   <Project />
-    //   <Utilities />
-    //   <Design />
-    //   <Sungroup />
-    //   <Form />
-    // </Layout>
-    <p>next</p>
+    <Layout>
+      <Slider />
+      <AboutKemBeach />
+      <BillComment />
+      <Project />
+      <Utilities />
+      <Design />
+      <Sungroup />
+      <Form />
+    </Layout>
   );
 }
